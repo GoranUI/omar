8594a3fc6d21b0b1dfea333082d1c9b12065e3b0
GoranUI <petkovicg001@gmail.com>
2025-07-15 03:43:51 +0200

    reshavam srkijev kod

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,16 +24,16 @@ module.exports = {
         "soft-bounce": {
           "0%, 100%": {
             transform: "translateY(0)",
-            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
+            animationTimingFunction: "ease-in-out",
           },
           "50%": {
-            transform: "translateY(-8px)", // soft bounce
-            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+            transform: "translateY(-8px)",
+            animationTimingFunction: "ease-in-out",
           },
         },
       },
       animation: {
-        "soft-bounce": "soft-bounce 1s infinite",
+        "soft-bounce": "soft-bounce 3s ease-in-out infinite",
       },
     },
   },
